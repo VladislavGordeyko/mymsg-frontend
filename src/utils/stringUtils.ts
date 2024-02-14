2083bd87858c0a9d7c67df6cfedc1b6f67b5da3d
Vladislav Gordeyko <vladislavgordeyko@Vladislavs-MacBook-Air.local>
2024-02-14 05:06:21 +0400

    check

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,6 +1,7 @@
 export const getTitleByTgUserName = (tgUserName: string) => {
   // const userNamesMapRaw = process.env.NEXT_PUBLIC_USERNAMES_MAP as string;
   const userNamesMapRaw = process.env.USERNAMES_MAP as string;
+  console.log({ userNamesMapRaw }, { tgUserName })
 
   let userNamesMap: { username: string, title: string }[] = [];
   if (userNamesMapRaw) {
